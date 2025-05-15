b41bc41031586df16db915058a48c232c6b59f81
Trenton Cochell <trentoncochell@gmail.com>
2025-05-15 14:44:55 -0500

    Added some code for encoding but got stuck on the writing of the bits

--- src/main/java/edu/grinnell/csc207/compression/HuffmanTree.java
+++ src/main/java/edu/grinnell/csc207/compression/HuffmanTree.java
@@ -1,6 +1,7 @@
 package edu.grinnell.csc207.compression;
 
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.PriorityQueue;
@@ -19,35 +20,34 @@ import java.util.PriorityQueue;
 public class HuffmanTree {
 
     public class Node implements Comparable<Node> {
-    short value;
-    int freq;
-    Node leftChild, rightChild;
 
-    Node(short value, int freq) {
-        this.value = value;
-        this.freq = freq;
+        short value;
+        int freq;
+        Node leftChild, rightChild;
+
+        Node(short value, int freq) {
+            this.value = value;
+            this.freq = freq;
+        }
+
+        // Should create the internal node?
+        Node(Node leftChild, Node rightChild) {
+            this.leftChild = leftChild;
+            this.rightChild = rightChild;
+            this.freq = leftChild.freq + rightChild.freq;
+        }
+
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.freq, other.freq);
+        }
+
+        boolean isLeaf() {
+            return leftChild == null && rightChild == null;
+        }
     }
 
-    // Should create the internal node?
-    Node(Node leftChild, Node rightChild) {
-        this.leftChild = leftChild;
-        this.rightChild = rightChild;
-        this.freq = leftChild.freq + rightChild.freq;
-    }
-
-
-    @Override
-    public int compareTo(Node other) {
-        return Integer.compare(this.freq, other.freq);
-    }
-
-    boolean isLeaf() {
-        return leftChild == null && rightChild == null;
-    }
-}
-    
     private Node root;
-
 
     /**
      * Constructs a new HuffmanTree from a frequency map.
@@ -58,20 +58,20 @@ public class HuffmanTree {
 
         //theoretically creates a priority queue ordered in ascending order by value
         PriorityQueue<Node> queue = new PriorityQueue<>();
-        
+
         for (Map.Entry<Short, Integer> entry : freqs.entrySet()) {
             queue.add(new Node(entry.getKey(), entry.getValue()));
         }
-   
+
         while (queue.size() >= 2) {
             //create a new node using the top two nodes of the queue as children
-            Node firstval = queue.poll();
+            Node firstval = queue.poll(); //or is this peek?
             Node secondval = queue.poll();
             //add this node back into the queue
             Node parent = new Node(firstval, secondval);
             queue.add(parent);
         }
-        
+
         //To set the root
         if (!queue.isEmpty()) {
             root = queue.poll();
@@ -99,6 +99,34 @@ public class HuffmanTree {
     }
 
     /**
+     * A table of the mappings of leaf values to bits
+     *
+     * @table A map of shorts(the bits) and chars they represent
+     */
+    private Map<Short, String> Table(Node root) {
+        Map<Short, String> bitStringtable = new HashMap<>();
+        Recurser(root, "", bitStringtable);
+        return bitStringtable;
+    }
+
+    /**
+     * A helper method for Table to recursively add 0s and 1s
+     */
+    private void Recurser(Node node, String path, Map<Short, String> table) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.isLeaf()) {
+            table.put(node.value, path);
+            return;
+        }
+
+        Recurser(node.leftChild, path + "0", table); //left is 0
+        Recurser(node.rightChild, path + "1", table); // right is 1
+    }
+
+    /**
      * Encodes the file given as a stream of bits into a compressed format using
      * this Huffman tree. The encoded values are written, bit-by-bit to the
      * given BitOuputStream.
@@ -107,7 +135,18 @@ public class HuffmanTree {
      * @param out the file to write the compressed output to.
      */
     public void encode(BitInputStream in, BitOutputStream out) {
-        // TODO: fill me in!
+        Map<Short, String> encodingTable = Table(root);
+
+        int value;
+        while ((value = in.readBit()) != -1) {
+            short s = (short) value;
+            String code = encodingTable.get(s);
+
+            for (char bit : code.toCharArray()) {
+                //some sort of traversal and writing of the bits (but there is
+                //no method for that...)
+            }
+        }
     }
 
     /**
